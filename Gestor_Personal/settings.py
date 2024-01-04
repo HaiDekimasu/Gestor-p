c49f5c2544b2e2a473f4d833be59c1768c6dc68d
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-04 10:46:24 -0300

    modified:   Gestor_Personal/__pycache__/settings.cpython-311.pyc 	modified:   Gestor_Personal/settings.py

--- Gestor_Personal/settings.py
+++ Gestor_Personal/settings.py
@@ -117,11 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-BASE_DIR = '/home/jcadev/Gestor-p'
+# BASE_DIR = '/home/jcadev/Gestor-p'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 
 
 # Default primary key field type
