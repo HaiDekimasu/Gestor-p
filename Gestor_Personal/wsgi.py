298dd56e1cb728fa06893d8b83ae6dc0ed686872
Haidekimasu <cadevillajefferson@gmail.com>
2024-01-05 09:45:48 -0300

    modified:   .vscode/settings.json 	modified:   Gestor_Personal/__pycache__/settings.cpython-311.pyc 	modified:   Gestor_Personal/__pycache__/wsgi.cpython-311.pyc 	modified:   Gestor_Personal/wsgi.py

--- Gestor_Personal/wsgi.py
+++ Gestor_Personal/wsgi.py
@@ -1,10 +1,10 @@
-# import os
-# import sys
+import os
+import sys
 
-# path = os.path.expanduser('~/Gestor-p/Gestor_Personal')
-# if path not in sys.path:
-#     sys.path.insert(0, path)
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'Gestor_Personal.settings'
-# from django.core.wsgi import get_wsgi_application
-# from django.contrib.staticfiles.handlers import StaticFilesHandler
-# application = StaticFilesHandler(get_wsgi_application())
+path = os.path.expanduser('~/Gestor-p/Gestor_Personal')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Gestor_Personal.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
